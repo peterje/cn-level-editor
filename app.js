a9be368796a4bea0343c0970f646ee1a505c816c
Peter <petejedmonds@gmail.com>
2020-06-25 00:30:08 -0700

    Added export button

--- app.js
+++ app.js
@@ -1,35 +1,87 @@
 var sceneObjects = []
 var canvas = new fabric.Canvas('c');
-// create a rectangle object
-var rect = new fabric.Rect({
-  left: 100,
-  top: 100,
-  fill: 'red',
-  width: 20,
-  height: 20
-});
+fabric.Group.prototype.lockScalingX = true;
+fabric.Group.prototype.lockScalingY = true;
+canvas.preserveObjectStacking = true;
 
 document.querySelectorAll('.library img').forEach(el => {
-  el.addEventListener("click", ()=>{
-    fabric.Image.fromURL(el.src, (img)=>{
-      canvas.add(img);
+  el.addEventListener("click", () => {
+    fabric.Image.fromURL(el.src, (img) => {
+      var obj = canvas.add(img);
+      obj.lockUniScaling = true;
+      console.log(canvas.getObjects());
     });
   });
 });
 
-var objectDictionary = {
-    "background":["background", "background.png"],
-    "lava":["damage", "floor3.png"],
-    "grass":["solid", "floor2.png"],
-    "wood":["solid", "plank.png"],
-    "coin":["coin","coin.png"],
-    "chest":["chest","chestopen.png"],
-    "tree":["background","tree3.png"],
+canvas.onBeforeScaleRotate = function lock(object) {
+  object.set({ lockScalingX: false, lockScalingY: false });
+};
+var spriteToType =
+{
+  "background.png": "background",
+  "tree3.png": "background",
+  "floor2.png": "solid",
+  "chestopen.png": "chest",
+  "floor3.png": "damage",
+  "plank.png": "solid",
+  "coin.png": "coin",
+  "lava.png": "damage",
+}
+function exportToJson() {
+  var world = new Object();
+  var objects = [];
+  canvas.getObjects().forEach(el => {
+    var spriteImage = el._originalElement.currentSrc.replace(/^.*[\\\/]/, '');
+    if (spriteImage == "1.png") {
+      var hero =
+      {
+        x: el.left,
+        y: el.top,
+        vx: 0,
+        vy: 0,
+        w: el.width * el.scaleX,
+        h: el.height * el.scaleY,
+        og: false
+      }
+      world.hero = hero;
+    }else{
+    var sceneObject =
+    {
+      x: el.left,
+      y: el.top,
+      w: el.width * el.scaleX,
+      h: el.height * el.scaleY,
+      sprite: spriteImage,
+      type: spriteToType[spriteImage]
     }
-function addObject(type){
-    var objectType = objectDictionary[type][0];
-    var objectSprite = objectDictionary[type][1];
-    var image = new Image();
-    image.src = "assets/" + objectSprite;
-    sceneObjects.push({x:0, y:0, w:image.width, h:image.height, type:objectType, sprite:objectSprite})
+    objects.push(sceneObject);
+    }
+  });
+  world.objects = objects;
+  world.code = "mc100";
+  console.log(JSON.stringify(world));
+}
+function showInfo() {
+  var object = canvas.getActiveObject();
+  console.log(object.left);
+}
+function deleteSelected() {
+  canvas.remove(canvas.getActiveObject());
+  console.log(canvas.getObjects());
+}
+function deleteAll() {
+  canvas.remove(canvas.getObjects());
+}
+function moveToTop() {
+  canvas.bringToFront(canvas.getActiveObject());
+}
+function moveToBottom() {
+  canvas.sendToBack(canvas.getActiveObject());
+}
+function moveUpLayer() {
+  canvas.bringForward(canvas.getActiveObject());
+}
+function moveDownLayer() {
+  canvas.sendBackwards(canvas.getActiveObject());
 }
