44f3d4202de23105bda2917cc2ab63b4f01d6d8b
Peter Edmonds <peter@pop-os.localdomain>
2021-06-21 11:24:12 -0700

    changed flexbox layout

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var canvas = new fabric.Canvas('c');
+var _clipboard;
 canvas.preserveObjectStacking = true;
 
-document.querySelectorAll('.library img').forEach(el => {
+document.querySelectorAll('img').forEach(el => {
 	el.addEventListener("click", () => {
+		console.log("clicked")
 		fabric.Image.fromURL(el.src, (img) => {
 			var obj = canvas.add(img);
 			obj.lockUniScaling = true;
@@ -10,6 +12,7 @@ document.querySelectorAll('.library img').forEach(el => {
 		});
 	});
 });
+
 
 var spriteToType =
 {
@@ -22,6 +25,45 @@ var spriteToType =
 	"coin.png": "coin",
 	"lava.png": "damage",
 }
+
+
+function Copy() {
+	// clone what are you copying since you
+	// may want copy and paste on different moment.
+	// and you do not want the changes happened
+	// later to reflect on the copy.
+	canvas.getActiveObject().clone(function(cloned) {
+		_clipboard = cloned;
+	});
+}
+
+function Paste() {
+	// clone again, so you can do multiple copies.
+	_clipboard.clone(function(clonedObj) {
+		canvas.discardActiveObject();
+		clonedObj.set({
+			left: clonedObj.left + clonedObj.width,
+			top: clonedObj.top,
+			evented: true,
+		});
+		if (clonedObj.type === 'activeSelection') {
+			// active selection needs a reference to the canvas.
+			clonedObj.canvas = canvas;
+			clonedObj.forEachObject(function(obj) {
+				canvas.add(obj);
+			});
+			// this should solve the unselectability
+			clonedObj.setCoords();
+		} else {
+			canvas.add(clonedObj);
+		}
+		_clipboard.left += _clipboard.width;
+		canvas.setActiveObject(clonedObj);
+		canvas.requestRenderAll();
+	});
+}
+
+
 function exportToJson() {
 	var world = new Object();
 	var objects = [];
